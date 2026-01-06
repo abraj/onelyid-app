337f39d371a0d5df436a1a764eba16f4d9311b25
Abhishek Raj <abhi@raj.me>
2026-01-07 01:01:45 +0700

    Use oauth client middleware

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import events from 'node:events'
 import type http from 'node:http'
 import express, { type Express } from 'express'
 import { pino } from 'pino'
+import { authMiddleware, type AuthMiddlewareConfig } from '@onelyid/client'
 
 import { env } from '#/lib/env'
 import { createRouter } from '#/routes'
@@ -28,6 +29,16 @@ export class Server {
     // Routes & middlewares
     const router = createRouter(ctx)
 
+    const config: AuthMiddlewareConfig = {
+      // dbPath: env.DB_PATH,
+      // cookieSecret: env.COOKIE_SECRET,
+      // publicUrl: env.PUBLIC_URL,
+      // logger,
+      // loginRedirect: '/',
+    };
+    app.use(authMiddleware(config))
+
+    // NOTE: parse body after auth middleware
     app.use(express.json())
     app.use(express.urlencoded({ extended: true }))
 
